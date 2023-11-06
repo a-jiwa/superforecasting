09b7155846beaea1259d405d1f855b48d5cb6a3b
adnaanjiwa <adnaanjiwa@gmail.com>
2023-11-06 10:16:29 +0000

    Layout for landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Verification from "./components/Verification";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>; // Or some loading spinner component
+    }
+
+    return (
+        <Router>
+            <Routes>
+                {/* If user is logged in but not verified, navigate to Verification page */}
+                <Route path="/" element={user ? (user.emailVerified ? <Home /> : <Verification setUser={setUser} />) : <Landing />} />
+                <Route path="/login" element={user ? (user.emailVerified ? <Navigate to="/" /> : <Navigate to="/verification" />) : <Login />} />
+                <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+                {/* Only navigate to Verification if the user is logged in and not verified */}
+                <Route path="/verification" element={user ? (user.emailVerified ? <Navigate to="/" /> : <Verification setUser={setUser} />) : <Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
