9452af7292c16baa1458774f55e5f662afd32a0d
adnaanjiwa <adnaanjiwa@gmail.com>
2023-12-27 12:52:10 +0000

    gh pages configuration

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ function App() {
             <Routes>
                 {/* If user is logged in but not verified, navigate to Verification page */}
                 <Route path="/" element={user ? (user.emailVerified ? <Home /> : <Verification setUser={setUser} />) : <Landing />} />
-                <Route path="login" element={user ? (user.emailVerified ? <Navigate to="/" /> : <Navigate to="/verification" />) : <Login />} />
-                <Route path="signup" element={user ? <Navigate to="/" /> : <Signup />} />
+                <Route path="/login" element={user ? (user.emailVerified ? <Navigate to="/" /> : <Navigate to="/verification" />) : <Login />} />
+                <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
                 {/* Only navigate to Verification if the user is logged in and not verified */}
-                <Route path="verification" element={user ? (user.emailVerified ? <Navigate to="/" /> : <Verification setUser={setUser} />) : <Navigate to="/login" />} />
+                <Route path="/verification" element={user ? (user.emailVerified ? <Navigate to="/" /> : <Verification setUser={setUser} />) : <Navigate to="/login" />} />
             </Routes>
         </Router>
     );
