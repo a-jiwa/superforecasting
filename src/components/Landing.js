0c9a6cb2658b0aeb835844e45c15e4582a6f740e
adnaanjiwa <adnaanjiwa@gmail.com>
2023-12-31 14:23:46 +0000

    styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -29,22 +29,24 @@ function Landing() {
                     {/*    <ShapeAnimation />*/}
                     {/*</div>*/}
                     <div className="text-area">
-                    {/* Lottie Animation */}
-                    {/*<Lottie options={defaultOptions} className="lottie-animation" />*/}
                     <h3>Make your forecasts today.</h3>
-                        <p className="landing-p-text">Research has found that some people are exceptionally skilled at assigning realistic
-                            probabilities to possible outcomes – even on topics outside their expertise or interest.
-                            These people are called <a href="https://goodjudgment.com/about/"
-                                                       className="superforecaster-link">Superforecasters</a>.</p>
-                        <p className="landing-p-text">Are you one of these Superforecasters?</p>
-                        <p className="landing-p-text">Compete against a pool of rivals for a <b className="landing-b-text">£100 Amazon voucher</b>,
-                            answering questions on the year ahead covering categories including Politics, Popular Culture and Sport to find out just how good at political and social punditry you really are!</p>
+                        <div className="text-area-bigscreen">
+                            <p className="landing-p-text">Research has found that some people are exceptionally skilled at assigning realistic
+                                probabilities to possible outcomes – even on topics outside their expertise or interest.
+                                These people are called <a href="https://goodjudgment.com/about/"
+                                                           className="superforecaster-link">Superforecasters</a>.</p>
+                            <p className="landing-p-text">Are you one of these Superforecasters?</p>
+                            <p className="landing-p-text">Compete against a pool of rivals for a <b className="landing-b-text">£100 Amazon voucher</b>, answering questions from global politics to popular culture in  <b className="landing-b-text"> the year ahead</b>, to find out!</p>
+                        </div>
+                        <div className="text-area-smallscreen">
+                            <p className="landing-p-text">Compete against a pool of rivals for a <b className="landing-b-text">£100 amazon voucher</b>, answering
+                                questions from global politics to popular culture in  <b className="landing-b-text"> the year ahead</b>. Find out just
+                                how good at political and social punditry you really are!</p>
+                        </div>
                     </div>
                 </div>
                 <div className="right-column">
 
-                    {/*<h3>Make your forecasts now!</h3>*/}
-                    {/*<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>*/}
                     <div className="buttons">
                         <Link to="/signup" className="button blue-button">
                             Get Started
